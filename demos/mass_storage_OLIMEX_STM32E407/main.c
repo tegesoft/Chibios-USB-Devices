baa8e93ed23b3f5b0dcca653dee8bd651dbe06a0
Laurent Gomila <laurent.gom@tegesoft.com>
2013-04-19 11:20:16 +0200

    Updated the demo

--- demos/mass_storage_OLIMEX_STM32E407/main.c
+++ demos/mass_storage_OLIMEX_STM32E407/main.c
@@ -1,40 +1,82 @@
+#include "usb_msd.h"
+#include "ch.h"
+#include "hal.h"
 #include <stdio.h>
 #include <string.h>
 
-#include "ch.h"
-#include "hal.h"
+/* USB identifiers and strings that we'll send to the host */
+MSD_DECLARE_DEVICE_DESCRIPTOR(usbDeviceDescriptor, 0x0483, 0x5742);
 
-#include "usb_msd.h"
+#define USB_VENDOR_STRING \
+    'D', 0, 'e', 0, 'm', 0, 'o', 0, 'V', 0, 'e', 0, 'n', 0, 'd', 0, 'o', 0, 'r', 0
+MSD_DECLARE_STRING_DESCRIPTOR(usbVendorDescriptor, 20, USB_VENDOR_STRING);
 
-/*
- * Green LED blinker thread, times are in milliseconds.
- */
-static WORKING_AREA(waThread1, 128);
-static msg_t Thread1(void *arg) {
+#define USB_PRODUCT_STRING \
+    'D', 0, 'e', 0, 'm', 0, 'o', 0, 'P', 0, 'r', 0, 'o', 0, 'd', 0, 'u', 0, 'c', 0, 't', 0
+MSD_DECLARE_STRING_DESCRIPTOR(usbProductDescriptor, 22, USB_PRODUCT_STRING);
 
-  (void)arg;
-  chRegSetThreadName("blinker");
-  while (TRUE) {
-    palTogglePad(GPIOC, GPIOC_LED);
-    chThdSleepMilliseconds(500);
-  }
+#define USB_SERIAL_NUMBER_STRING \
+    '0', 0, '0', 0, '0', 0, '0', 0, '0', 0, '0', 0, '0', 0, '0', 0, '0', 0, '0', 0, '0', 0, '1', 0
+MSD_DECLARE_STRING_DESCRIPTOR(usbSerialNumberDescriptor, 24, USB_SERIAL_NUMBER_STRING);
+
+/* Turns on a LED when there is I/O activity on the USB port */
+static void usbActivity(bool_t on)
+{
+    if (on)
+        palSetPad(GPIOC, GPIOC_LED);
+    else
+        palClearPad(GPIOC, GPIOC_LED);
 }
 
-int main(void) {
-  halInit();
-  chSysInit();
+/* USB mass storage configuration */
+static USBMassStorageConfig msdConfig =
+{
+    &USBD2,
+    (BaseBlockDevice*)&SDCD1,
+    usbActivity,
+    &usbDeviceDescriptor,
+    &usbVendorDescriptor,
+    &usbProductDescriptor,
+    &usbSerialNumberDescriptor
+};
 
-  sdcStart(&SDCD1, NULL);
-  sdcConnect(&SDCD1);
+/* USB mass storage driver */
+USBMassStorageDriver UMSD1;
 
-  //palSetPad(GPIOC, GPIOC_LED);
+int main(void)
+{
+    /* system & hardware initialization */
+    halInit();
+    chSysInit();
 
-  USBMassStorageDriver UMSD1;
-  msdInit(&USBD1, &SDCD1, &UMSD1);
+    /* initialize the SD card */
+    sdcStart(&SDCD1, NULL);
+    sdcConnect(&SDCD1);
 
-  chThdCreateStatic(waThread1, sizeof(waThread1), NORMALPRIO, Thread1, NULL);
+    /* turn off the test LED */
+    palClearPad(GPIOC, GPIOC_LED);
 
-  while (TRUE) {
-    chThdSleepMilliseconds(1000);
-  }
+    /* run the USB mass storage service */
+    msdInit(&UMSD1, &msdConfig);
+
+    /* watch the mass storage events */
+    EventListener connected;
+    EventListener ejected;
+    chEvtRegisterMask(&UMSD1.evt_connected, &connected, EVENT_MASK(1));
+    chEvtRegisterMask(&UMSD1.evt_ejected, &ejected, EVENT_MASK(2));
+
+    while (TRUE)
+    {
+        eventmask_t event = chEvtWaitOne(EVENT_MASK(1) | EVENT_MASK(2));
+        if (event == EVENT_MASK(1))
+        {
+            /* media connected */
+        }
+        else if (event == EVENT_MASK(2))
+        {
+            /* media ejected */
+        }
+    }
+
+    return 0;
 }
--- demos/mass_storage_OLIMEX_STM32E407/readme.md
+++ demos/mass_storage_OLIMEX_STM32E407/readme.md
@@ -1,5 +1,3 @@
 Mass Storage Demo for Olimex STM32E407
 
 Uses SDC card + HS USB
-
-Note: not yet functional
